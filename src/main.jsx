d6f0a8855918d74e721d09d29ca2cc4957b3fcbc
junayet <muhammadjunayetmaruf@gmail.com>
2024-03-31 16:06:36 +0600

    router and redux has setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './Redux/store.jsx'
+import { MyRouts } from './MyRouts/MyRouts.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+    <RouterProvider router={MyRouts} />
+    </React.StrictMode>
+  </Provider>,
 )
